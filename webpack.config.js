6b854a45b477f0b65048c1d775e4a29698ed6870
Maximilien Mellen <maxmellen0@gmail.com>
2020-08-21 15:41:05 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+let path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+};
